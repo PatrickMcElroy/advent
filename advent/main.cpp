8485410a821252f1398fd8310c7d621adfece5de
Highlowkey <pdmcelroy1001@gmail.com>
2020-12-02 00:11:18 -0500

    Initial Commit

---
+++ advent/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  advent
+//
+//  Created by Patrick McElroy on 12/2/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
